5d6f71f54384aedbfdcb6194265aa57aab6448d8
sondarr <io.vavilov@gmail.com>
2023-02-24 16:00:07 +0700

    ДЗ от 16.02.2023

--- OOP.py
+++ OOP.py
@@ -4190,10 +4190,10 @@
 # ================================
 # Lesson 16.02.2023
 
-import sqlite3
-
-with sqlite3.connect('users.db') as con:
-    cur = con.cursor()
+# import sqlite3
+#
+# with sqlite3.connect('users.db') as con:
+#     cur = con.cursor()
 
 
     # cur.execute("""
@@ -4228,5 +4228,31 @@ with sqlite3.connect('users.db') as con:
     # age INTEGER NOT NULL CHECK(age>15 AND age<70),
     # email TEXT UNIQUE
     # )""")
+
+# ====================================
+# Урок от 21.02.2023
+
+# import sqlite3
+#
+# with sqlite3.connect('db_3.db') as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     SELECT *
+#     FROM T1
+#     ORDER BY FName
+#     LIMIT 2, 5
+#     """)
+#
+#     res = cur.fetchone()
+#     print(res)
+#
+#     res2 = cur.fetchmany(5)
+#     print(res2)
+
+    # res = cur.fetchall()
+    # print(res)
+
+    # for res in cur:
+    #     print(res)
 
 
